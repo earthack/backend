e2f7027d8d92b9098eddae4d9cf126e598f02c09
vardaro <anthonyavardaro@yahoo.com>
2018-04-21 18:07:30 -0700

    begin server and make some endpoints

---
+++ secret.js
@@ -0,0 +1,3 @@
+module.exports = {
+    api: 'AIzaSyAAn9ArDDiDRoRN5Z0k-7Ml4wjfz7zkxyE'
+}
--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 const request = require('request');
+const express = require('express');
+const {api} = process.env.API || require('./secret.js');
+
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+let port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.static(`client`));
+
+app.post('/api/midpoint', (req, res) => {
+    let numBuses = req.body.buses;
+    let locations = req.body.locations;
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('./client/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
