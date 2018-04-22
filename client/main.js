89ad445d708fade46e42c5d3a3591f3eaf1f85d7
vardaro <anthonyavardaro@yahoo.com>
2018-04-21 22:36:54 -0700

    fff

--- client/main.js
+++ client/main.js
@@ -1,10 +1,20 @@
 $(document).ready(() => {
-    console.log('he');
-    console.log(google);
-
     let addressArray = [];
-
+    let buses = 0;
+    console.log(google);
     const geocoder = new google.maps.Geocoder;
+        
+    //number of clusters, defaults to undefined
+    clusterMaker.k(3);
+    
+    //number of iterations (higher number gives more time to converge), defaults to 1000
+    clusterMaker.iterations(750);
+    
+    //data from which to identify clusters, defaults to []
+    clusterMaker.data([[1, 0], [0, 1], [0, 0], [-10, 10], [-9, 11], [10, 10], [11, 12]]);
+    
+    console.log(clusterMaker.clusters());
+
     $("#add").click(e => {
         let curInput = $('#address').val().trim();
         
@@ -18,6 +28,7 @@ $(document).ready(() => {
         }, (res, status) => {
             // append the res to the list showing address
             if (status == "OK") {
+
                 addressArray.push(res[0]);
                 $('#address').val('')
                 rebuildBox();
@@ -26,23 +37,9 @@ $(document).ready(() => {
     });
 
     $('#submit').click(() => {
-        let buses = $('#numBuses').val();
-        // send to api
-        let server = {
-            buses: buses,
-            locations: addressArray
-        };
-        console.log(server);
+        buses = $('#numBuses').val();
 
-        $.ajax({
-            type: "POST",
-            url: '/api/midpoint',
-            data: data,
-            success: (data) => {
-                console.log(data);
-
-            },
-        });
+        let a = calcMidpoint();
     });
     function rebuildBox() {
         let len = addressArray.length;
@@ -54,5 +51,56 @@ $(document).ready(() => {
         }
         // set the box to str
         $('#locations').html(str);
+    }
+
+    function calcMidpoint() {
+        let midLat = 0;
+        let midLong = 0;
+    
+        let x = 0;
+        let y = 0;
+        let z = 0;
+    
+        // temp lat long for loop
+        let tempLat, tempLong;
+        // temp cartesian coordinates
+        let tempX, tempY, tempZ;
+    
+        for (let i = 0; i < addressArray.length; i++) {
+            let curLocation = addressArray[i];
+            tempLat = curLocation.geometry.location.lat() * (Math.PI/180);
+            tempLong = curLocation.geometry.location.lng() * (Math.PI/180);
+
+            tempX = Math.cos(tempLat) * Math.cos(tempLong);
+            tempY = Math.cos(tempLat) * Math.sin(tempLong);
+            tempZ = Math.sin(tempLat);
+
+            x += tempX;
+            y += tempY;
+            z += tempZ;
+        }
+
+        x = x/addressArray.length;
+        y = y/addressArray.length;
+        z = z/addressArray.length;
+        midLong = Math.atan2(y,x);
+        let hypotenuse = Math.sqrt(x * x + y * y);
+        midLat = Math.atan2(z, hypotenuse);
+
+        midLat = toDegFromRad(midLat);
+        midLong = toDegFromRad(midLong);
+        console.log(midLat);
+        console.log(midLong);
+        // now we have the coordinates of the midpoint!
+        let point = new google.maps.LatLng(midLat, midLong);
+        console.log(point.toJSON());
+
+    }
+    function toDegFromRad(rd) {
+        return (rd * 180/Math.PI);
+    }
+
+    function initMap() {
+
     }
 });
